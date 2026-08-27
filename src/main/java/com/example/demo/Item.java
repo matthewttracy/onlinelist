2c0c31896c2da04261ea8b1b240e970b9003a300
Matthew <65zb5w66qc@privaterelay.appleid.com>
2026-08-27 17:48:22 +0100

    added post /items

--- src/main/java/com/example/demo/Item.java
+++ src/main/java/com/example/demo/Item.java
@@ -11,6 +11,10 @@ public class Item {
         this.done = false;
     }
 
+    public Item() {
+        
+    }
+
     public int getId(){
         return id;
     }
--- src/main/java/com/example/demo/ItemsController.java
+++ src/main/java/com/example/demo/ItemsController.java
@@ -4,15 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ItemsController {
 
-    List<Item> li = new ArrayList<>();
+    private List<Item> li = new ArrayList<>();
 
     @GetMapping("/items")
     public List<Item> list(){
         return li;
     }
+
+    @PostMapping("/items")
+    public Item add(@RequestBody Item item){
+        li.add(item);
+        return item;
+    }
+
 }
